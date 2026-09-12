02a74d0d77e53125bee91e565b43c5e91ea4aa50
kingreniel <kingmalate8@gmail.com>
2026-09-12 20:56:43 +0800

    Initial Commit

---
+++ src/components/CameraComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>Camera</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      <ion-button expand="block" @click="takePicture">
+        <ion-icon slot="start" :icon="cameraIcon" />
+        Take Picture
+      </ion-button>
+
+      <ion-text v-if="errorMessage" color="danger">
+        <p>{{ errorMessage }}</p>
+      </ion-text>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script setup lang="ts">
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonIcon,
+  IonText,
+} from "@ionic/vue";
+
+import { camera as cameraIcon } from "ionicons/icons";
+
+import { Camera, CameraResultType } from "@capacitor/camera";
+
+import { ref } from "vue";
+
+const errorMessage = ref("");
+
+const emit = defineEmits<{
+  (event: "photoCaptured", photo: string): void;
+}>();
+
+const takePicture = async () => {
+  errorMessage.value = "";
+
+  try {
+    const photo = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+    });
+
+    if (photo.webPath) {
+      emit("photoCaptured", photo.webPath);
+    }
+  } catch (error) {
+    console.error(error);
+    errorMessage.value = "Unable to capture photo.";
+  }
+};
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,36 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>My Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding">
+      <CameraComponent @photo-captured="addPhoto" />
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <PhotoGalleryComponent :photos="photos" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+
+import { ref } from "vue";
+
+import CameraComponent from "@/components/CameraComponent.vue";
+import PhotoGalleryComponent from "@/components/PhotoGalleryComponent.vue";
+
+const photos = ref<string[]>([]);
+
+const addPhoto = (photo: string) => {
+  photos.value.unshift(photo);
+};
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
